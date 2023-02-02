8d076e1539b116f758b91caae10de69d205d5103
StaphanieKing <staphaniek@yahoo.com>
2023-02-02 03:14:50 +0000

    inital commit login

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,14 +2,33 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS, cross_origin
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+
 
 api = Blueprint('api', __name__)
 CORS(api)
 
+
 @api.route('/hello', methods=['POST', 'GET'])
 def handle_hello():
-
-    response_body = jsonify(message="Simple server is running")
-    response_body.headers.add("Access-Control-Allow-Origin", "*")
+    response_body = jsonify(message='Simple server is running')
+    response_body.headers.add('Access-Control-Allow-Origin', '*')
     return response_body, 200
+
+
+
+
+# Create a route to authenticate your users and return JWTs. The
+# create_access_token() function is used to actually generate the JWT.
+@api.route("/login", methods=["POST"])
+def login():
+    username = request.json.get("email", None)
+    password = request.json.get("password", None)
+    if email != "test" or password != "test":
+        return jsonify({"msg": "Bad username or password"}), 401
+
+    access_token = create_access_token(identity=email)
+    return jsonify(access_token=access_token)
 
